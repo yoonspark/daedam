566276209a04c928c4ecc06a340d5d96f3c41595
Sangyoon Park <datumvitae@gmail.com>
2021-01-23 12:58:18 -0500

    Add utils for pagination

--- backend/config.py
+++ backend/config.py
@@ -6,3 +6,6 @@ DEBUG = True
 # DB connection
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.environ.get('DB_URI', 'postgres://localhost:5432/daedam_test')
+
+# Other envars
+ENTRIES_PER_PAGE = int(os.environ.get('ENTRIES_PER_PAGE', '10'))
---
+++ backend/utils.py
@@ -0,0 +1,9 @@
+from config import ENTRIES_PER_PAGE
+
+def paginate(request, entries: list) -> list:
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * ENTRIES_PER_PAGE
+    end = start + ENTRIES_PER_PAGE
+    current_entries = entries[start:end]
+
+    return current_entries
