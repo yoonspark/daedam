b449487bf3ebe5f008107372217fbd14737ad314
Sangyoon Park <datumvitae@gmail.com>
2021-01-24 12:35:02 -0500

    Set up unit tests

---
+++ backend/tests.py
@@ -0,0 +1,50 @@
+import os
+import json
+import unittest
+from flask_sqlalchemy import SQLAlchemy
+
+from app import create_app
+from models import (
+    setup_db,
+    Call,
+    Offer,
+    Panelist,
+    Topic,
+)
+
+
+class TriviaTestCase(unittest.TestCase):
+    def setUp(self):
+        """
+        Define test variables and initialize app.
+        """
+
+        self.app = create_app()
+        self.client = self.app.test_client
+        setup_db(self.app, 'postgres://localhost:5432/daedam_test')
+
+        # Bind the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+
+    def tearDown(self):
+        """
+        Executed after reach test.
+        """
+        pass
+
+    def test_retrieve_calls(self):
+        res = self.client().get('/calls')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsInstance(data['calls'], list)
+        self.assertIsInstance(data['total_calls'], int)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
