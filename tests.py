189c22942bdfd079ae93b332fad5ab79c3c8c06e
Sangyoon Park <datumvitae@gmail.com>
2021-01-28 10:59:05 -0500

    Create tests for endpoints involving offer records

--- tests.py
+++ tests.py
@@ -58,9 +58,31 @@ class DaedamTestCase(unittest.TestCase):
             'description': 'I want various scientific perspectives.',
             'topics': ['physics', 'chemistry', 'biology']
         }
+        self.new_offer = {
+            'title': 'How can love last long?',
+            'contents': 'Today, we observe increasing rates of divorce. Is lasting love just another social construct fabricated to sustain organized life? We shall discuss this timely question with three distinguished experts on love and relationship.',
+            'event_time': '2021-05-25T19:00:00',
+            'finalized': False,
+            'topics': ['philosophy', 'science', 'love'],
+            'panelists': ['Adam Sheck', 'Todd Creager', 'April Masini']
+        }
+        self.new_offer_title_only = {
+            'title': 'Can love last forever?',
+        }
+        self.new_offer_no_title = {
+            'contents': 'Today, we observe increasing rates of divorce. Is lasting love just another social construct fabricated to sustain organized life? We shall discuss this timely question with three distinguished experts on love and relationship.',
+            'event_time': '2021-05-25T19:00:00',
+            'finalized': False,
+            'topics': ['philosophy', 'science', 'love'],
+            'panelists': ['Adam Sheck', 'Todd Creager', 'April Masini']
+        }
 
     def tearDown(self):
         pass
+
+    #----------------------------------------------------------------------------#
+    # Calls
+    #----------------------------------------------------------------------------#
 
     # --- RETRIEVE ALL CALLS --- #
 
@@ -261,6 +283,215 @@ class DaedamTestCase(unittest.TestCase):
 
     def test_update_call_wrong_permission(self):
         res = self.client().patch('/calls/1', headers=self.headers_moderator, json=self.new_call_question_only)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Permission not found.')
+
+    #----------------------------------------------------------------------------#
+    # Offers
+    #----------------------------------------------------------------------------#
+
+    # --- RETRIEVE ALL OFFERS --- #
+
+    def test_retrieve_offers(self):
+        res = self.client().get('/offers', headers=self.headers_audience)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsInstance(data['offers'], list)
+        self.assertIsInstance(data['total_offers'], int)
+
+    def test_retrieve_offers_beyond_valid_page(self):
+        res = self.client().get('/offers?page=99999', headers=self.headers_audience)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsInstance(data['offers'], list)
+        self.assertEqual(len(data['offers']), 0)
+
+    def test_retrieve_offers_no_headers(self):
+        res = self.client().get('/offers')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Authorization header is expected.')
+
+    # --- CREATE NEW OFFER --- #
+
+    def test_create_offer(self):
+        res = self.client().post('/offers', headers=self.headers_moderator, json=self.new_offer)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'Offer record has been created successfully.')
+        self.assertIsInstance(data['id'], int)
+
+        # For reproducibility of DB, delete the created record
+        _ = self.client().delete(f'/offers/{data["id"]}', headers=self.headers_moderator)
+
+    def test_create_offer_no_body(self):
+        res = self.client().post('/offers', headers=self.headers_moderator)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        # self.assertEqual(data['message'], 'Request body is missing.')
+
+    def test_create_offer_no_title(self):
+        res = self.client().post('/offers', headers=self.headers_moderator, json=self.new_offer_no_title)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], '"title" is required in the request body.')
+
+    def test_create_offer_title_only(self):
+        res = self.client().post('/offers', headers=self.headers_moderator, json=self.new_offer_title_only)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'Offer record has been created successfully.')
+        self.assertIsInstance(data['id'], int)
+
+        # For reproducibility of DB, delete the created record
+        _ = self.client().delete(f'/offers/{data["id"]}', headers=self.headers_moderator)
+
+    def test_create_offer_no_headers(self):
+        res = self.client().post('/offers', json=self.new_offer)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Authorization header is expected.')
+
+    def test_create_offer_wrong_permission(self):
+        res = self.client().post('/offers', headers=self.headers_audience, json=self.new_offer)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Permission not found.')
+
+    # --- DELETE OFFER --- #
+
+    def test_delete_offer(self):
+        # Create a test record
+        res = self.client().post('/offers', headers=self.headers_moderator, json=self.new_offer)
+        data = json.loads(res.data)
+        offer_id = data["id"]
+
+        res = self.client().delete(f'/offers/{offer_id}', headers=self.headers_moderator)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'Offer record has been deleted successfully.')
+        self.assertEqual(data['id'], offer_id)
+
+    def test_delete_offer_not_exist(self):
+        res = self.client().delete('/offers/99999', headers=self.headers_moderator)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Offer record does not exist.')
+
+    def test_delete_offer_no_headers(self):
+        res = self.client().delete('/offers/3')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Authorization header is expected.')
+
+    def test_delete_offer_wrong_permission(self):
+        res = self.client().delete('/offers/3', headers=self.headers_audience)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Permission not found.')
+
+    # --- RETRIEVE SINGLE OFFER --- #
+
+    def test_retrieve_offer(self):
+        res = self.client().get('/offers/1', headers=self.headers_moderator)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsInstance(data['offers'], list)
+        self.assertEqual(data['total_offers'], 1)
+
+    def test_retrieve_offer_not_exist(self):
+        res = self.client().get('/offers/99999', headers=self.headers_moderator)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Offer record does not exist.')
+
+    def test_retrieve_offer_no_headers(self):
+        res = self.client().get('/offers/1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Authorization header is expected.')
+
+    # --- UPDATE OFFER --- #
+
+    def test_update_offer_title_only(self):
+        res = self.client().patch('/offers/1', headers=self.headers_moderator, json=self.new_offer_title_only)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'Offer record has been updated successfully.')
+        self.assertEqual(data['id'], 1)
+
+    def test_update_offer_no_title(self):
+        res = self.client().patch('/offers/1', headers=self.headers_moderator, json=self.new_offer_no_title)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['message'], 'Offer record has been updated successfully.')
+        self.assertEqual(data['id'], 1)
+
+    def test_update_offer_no_body(self):
+        res = self.client().patch('/offers/1', headers=self.headers_moderator)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        # self.assertEqual(data['message'], 'Request body is missing.')
+
+    def test_update_offer_not_exist(self):
+        res = self.client().patch('/offers/99999', headers=self.headers_moderator, json=self.new_offer_title_only)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Offer record does not exist.')
+
+    def test_update_offer_no_headers(self):
+        res = self.client().patch('/offers/1', json=self.new_offer_title_only)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Authorization header is expected.')
+
+    def test_update_offer_wrong_permission(self):
+        res = self.client().patch('/offers/1', headers=self.headers_audience, json=self.new_offer_title_only)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 401)
