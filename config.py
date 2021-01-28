1f78a7a032761a515413a995f144240356dcd108
Sangyoon Park <datumvitae@gmail.com>
2021-01-28 10:44:36 -0500

    Debug: Set empty string as default for JWT env vars to get correct error messages

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ AUTH0_ALGORITHMS = ['RS256']
 AUTH0_AUDIENCE = os.environ.get('AUTH0_AUDIENCE', 'daedam-api')
 
 # Tokens for testing
-JWT_AUDIENCE = os.environ.get('JWT_AUDIENCE')
-JWT_MODERATOR = os.environ.get('JWT_MODERATOR')
-JWT_ADMIN = os.environ.get('JWT_ADMIN')
+JWT_AUDIENCE = os.environ.get('JWT_AUDIENCE', '')
+JWT_MODERATOR = os.environ.get('JWT_MODERATOR', '')
+JWT_ADMIN = os.environ.get('JWT_ADMIN', '')
 
 # Other config variables
 ENTRIES_PER_PAGE = int(os.environ.get('ENTRIES_PER_PAGE', '10'))
